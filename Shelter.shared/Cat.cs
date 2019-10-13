c3ccc17d4dd51d53442b2d5cf5804d1ad4bfb254
kinoodaphne <daphne.kinoo@outlook.be>
2019-10-13 22:17:40 +0200

    Changed Cat.Clawed to Cat.Declawed

--- Shelter.shared/Cat.cs
+++ Shelter.shared/Cat.cs
@@ -9,13 +9,13 @@ namespace Shelter
 
         }
 
-        public Cat(int id, string name, string race, DateTime dateOfBirth, bool isChecked, bool kidFriendly, DateTime since, bool clawed) : base(id, name, dateOfBirth, isChecked, kidFriendly, since)
+        public Cat(int id, string name, string race, DateTime dateOfBirth, bool isChecked, bool kidFriendly, DateTime since, bool declawed) : base(id, name, dateOfBirth, isChecked, kidFriendly, since)
         {
             Race = race;
-            Clawed = clawed;
+            Declawed = declawed;
         }
 
         public string Race { get; set; }
-        public bool Clawed { get; set; }
+        public bool Declawed { get; set; }
     }
 }
--- Shelter.shared/Program.cs
+++ Shelter.shared/Program.cs
@@ -10,7 +10,7 @@ namespace Shelter.shared
 
             Cat cat1 = new Cat(1, "Jef", "Tabby", new DateTime(2009, 12, 06), true, true, new DateTime(2019, 10, 13), false);
             Console.WriteLine(cat1.Name);
-            Console.WriteLine("Declawed? " + cat1.Clawed);
+            Console.WriteLine("Declawed? " + cat1.Declawed);
             Console.WriteLine("---");
             Dog dog1 = new Dog(1, "Sia", "Shitzu", new DateTime(2019, 09, 09), true, true, new DateTime(2019, 10, 13), false);
             Console.WriteLine(dog1.Name);

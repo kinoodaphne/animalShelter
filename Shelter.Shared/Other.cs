cf1d1061f51260da58264ce2e0c4f85b46d491cc
kinoodaphne <daphne.kinoo@outlook.be>
2019-11-09 15:34:57 +0100

    Fixed part 1 of bugs in index.cshtml

--- Shelter.Shared/Other.cs
+++ Shelter.Shared/Other.cs
@@ -8,12 +8,12 @@ namespace Shelter.Shared
 
         }
 
-        public Other(int id, string name, string kind, string description, DateTime dateOfBirth, bool isChecked, bool kidFriendly, DateTime since) : base(id, name, dateOfBirth, isChecked, kidFriendly, since) {
-            Kind = kind;
+        public Other(int id, string name, string race, string description, DateTime dateOfBirth, bool isChecked, bool kidFriendly, DateTime since) : base(id, name, dateOfBirth, isChecked, kidFriendly, since) {
+            Race = race;
             Description = description;
         }
 
-        public string Kind { get; set; }
+        public string Race { get; set; }
         public string Description { get; set; }
     }
 }
